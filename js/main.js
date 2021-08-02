32349d6a98034509cd5be8841734162d0af2d9e7
rhdmldus <76199546+rhdmldus@users.noreply.github.com>
2021-08-02 22:46:55 +0900

    내용작성중

--- js/main.js
+++ js/main.js
@@ -1,129 +1,109 @@
 class ChangingTitle {
-constructor(x=null) {
+  constructor(x = null) {
     this.node = x;
-    this.letterfy(this.node.querySelector('h1'));
-}
-letterfy(node) {
+    this.letterfy(this.node.querySelector("h1"));
+  }
+  letterfy(node) {
     let text = node.innerText;
-    node.innerText = '';
-    node.classList.add('current');
+    node.innerText = "";
+    node.classList.add("current");
     for (let c in text) {
-    let span = document.createElement('span');
-    span.innerText = text[c];
-    span.classList.add('letter', 'in');
-    span.style.animationDelay = `${c * 0.1}s`;
-    node.appendChild(span);
+      let span = document.createElement("span");
+      span.innerText = text[c];
+      span.classList.add("letter", "in");
+      span.style.animationDelay = `${c * 0.1}s`;
+      node.appendChild(span);
     }
-}
-changeText(newText) {
-    let oldTitle = this.node.querySelector('.current');
+  }
+  changeText(newText) {
+    let oldTitle = this.node.querySelector(".current");
     let i = 0;
     for (let letter of oldTitle.children) {
-    letter.style.animationDelay = `${i++ * 0.1}s`;
-    letter.classList.remove('in');
-    letter.classList.add('out');
+      letter.style.animationDelay = `${i++ * 0.1}s`;
+      letter.classList.remove("in");
+      letter.classList.add("out");
     }
-    oldTitle.classList.remove('current');
-    let newTitle = document.createElement('h1');
-    newTitle.classList.add('current');
+    oldTitle.classList.remove("current");
+    let newTitle = document.createElement("h1");
+    newTitle.classList.add("current");
     for (let c in newText) {
-    let span = document.createElement('span');
-    span.innerText = newText[c];
-    span.classList.add('letter', 'in');
-    span.style.animationDelay = `${c * 0.1 + 0.5}s`;
-    newTitle.appendChild(span)
+      let span = document.createElement("span");
+      span.innerText = newText[c];
+      span.classList.add("letter", "in");
+      span.style.animationDelay = `${c * 0.1 + 0.5}s`;
+      newTitle.appendChild(span);
     }
     this.node.appendChild(newTitle);
     setTimeout(this.removeNode(oldTitle), 2000);
-}
-removeNode(x) {
+  }
+  removeNode(x) {
     return () => {
-    x.remove();
-    }
-}
-};
-
-let ct = new ChangingTitle(document.querySelector('.changing-title'));
-const texts = ['Creative', 'Innovative', 'flexible', 'Passionate'];
-let count = 0;
-setInterval(()=> {
-ct.changeText(texts[++count % texts.length]);
-}, 2000);
-
-// swiper
-// const swiper = new Swiper('.swiper-container', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     mousewheel: {
-//         invert: false,
-//     },
-//     // If we need pagination
-//     // pagination: {
-//     //     el: '.swiper-pagination',
-//     // },
-
-//     // // Navigation arrows
-//     // navigation: {
-//     //     nextEl: '.swiper-button-next',
-//     //     prevEl: '.swiper-button-prev',
-//     // },
-
-//     // // And if we need scrollbar
-//     // scrollbar: {
-//     //     el: '.swiper-scrollbar',
-//     // },
-//     });
-
-
-    let links = document.querySelectorAll('.item_list li a')
-    links.forEach(link => {
-        link.addEventListener('click',function(e){
-            e.preventDefault();
-            let target = e.currentTarget;
-            let idMoveTo = target.getAttribute('href')
-            let tagetElement = document.querySelector(`${idMoveTo}`)
-            tagetElement.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-            links.forEach(link => link.classList.remove('on'))
-            target.classList.add('on')
-
-    })
-})
-
-// https://codepen.io/zchee/pen/ogzvZZ
-
-
-
-window.onscroll = function(){
-    let header = document.querySelector('.nav')
-    let headerHeight = header.offsetHeight
-    let windowTop = window.scrollY
-    if(windowTop >= headerHeight) {
-        header.classList.add('drop')
-    }
-    else{
-        header.classList.remove('drop')
-    }
-}
-
-(function() {
-    'use strict';
-  
-    var section = document.querySelectorAll(".section");
-    var sections = {};
-    var i = 0;
-  
-    Array.prototype.forEach.call(section, function(e) {
-      sections[e.id] = e.offsetTop;
-    });
-  
-    window.onscroll = function() {
-      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-  
-      for (i in sections) {
-        if (sections[i] <= scrollPosition) {
-          document.querySelector('.active').setAttribute('class', ' ');
-          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'on');
-        }
-      }
+      x.remove();
     };
-  })
+  }
+}
+
+let ct = new ChangingTitle(document.querySelector(".changing-title"));
+const texts = ["Creative", "Innovative", "flexible", "Passionate"];
+let count = 0;
+setInterval(() => {
+  ct.changeText(texts[++count % texts.length]);
+}, 2000);
+//
+// let links = document.querySelectorAll(".item_list li a");
+// links.forEach((link) => {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let target = e.currentTarget;
+//     let idMoveTo = target.getAttribute("href");
+//     let tagetElement = document.querySelector(`${idMoveTo}`);
+//     tagetElement.scrollIntoView({
+//       behavior: "smooth",
+//       block: "end",
+//       inline: "nearest",
+//     });
+//     links.forEach((link) => link.classList.remove("on"));
+//     target.classList.add("on");
+//   });
+// });
+// headerdrop
+// window.onscroll = function () {
+//   let header = document.querySelector(".nav");
+//   let headerHeight = header.offsetHeight;
+//   let windowTop = window.scrollY;
+//   if (windowTop >= headerHeight) {
+//     header.classList.add("drop");
+//   } else {
+//     header.classList.remove("drop");
+//   }
+// };
+
+// scrollspy
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".nav .item_list .item a");
+window.onscroll = function () {
+  let header = document.querySelector(".nav");
+  let headerHeight = header.offsetHeight;
+  let windowTop = window.scrollY;
+  if (windowTop >= headerHeight) {
+    header.classList.add("drop");
+  } else {
+    header.classList.remove("drop");
+  }
+
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("on");
+        document
+          .querySelector(".nav .item_list .item a[href*=" + id + "]")
+          .classList.add("on");
+      });
+    }
+  });
+};
